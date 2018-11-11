a81f74140e050dcfdb6278dc880896108c06306a
kristoffer_bak <team16203@gitswat.ase.au.dk>
2018-11-11 21:55:05 +0100

    started adding NG foundation +auth stuff

--- app_server/models/user.js
+++ app_server/models/user.js
@@ -18,11 +18,34 @@ const userSchema = Schema({
     workouts: [{
         type: Schema.Types.ObjectId,
         ref: 'Workout'
-    }]
-    //,
+    }],
     
-   // hash: String,
-    //salt: String
+    
+    hash: String,
+    salt: String
 });
+
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64,'sha512').toString('hex');
+};
+
+userSchema.methods.validPassword = function(password){
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+    return this.hash === hash; //returns true or false, depending on whether the hashes match
+};
+
+userSchema.methods.generateJwt = function(){
+    var expiry = new Date();
+    expiry.setDate(expiry.getDate() + 7);
+
+    return jwt.sign({
+        _id: this. _id,
+        email: this.email,
+        name: this.name,
+       // workouts: new workouts(),
+        exp: parseInt(expiry.getTime() / 1000),},
+         process.env.JWT_SECRET);    
+};
 
 const User = mongoose.model('User', userSchema);
